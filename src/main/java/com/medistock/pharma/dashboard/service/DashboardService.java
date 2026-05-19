c38886d496686f2cd8cc29cb90fa4b56529191a3
shashankE20 <shashankejanthkar9@gmail.com>
2026-05-19 15:17:35 +0530

    Update DashboardService.java

--- src/main/java/com/medistock/pharma/dashboard/service/DashboardService.java
+++ src/main/java/com/medistock/pharma/dashboard/service/DashboardService.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class DashboardService {
+public class DashboardService   {
 
     private final MedicineRepository medicineRepository;
     private final SaleRepository saleRepository;
