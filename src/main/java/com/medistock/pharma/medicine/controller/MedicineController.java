5affe0918ec2c3da9b1bf38302db6ba2e0719498
shashankE20 <shashankejanthkar9@gmail.com>
2026-05-19 14:58:27 +0530

    Update MedicineController.java

--- src/main/java/com/medistock/pharma/medicine/controller/MedicineController.java
+++ src/main/java/com/medistock/pharma/medicine/controller/MedicineController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/medicines")
+@RequestMapping("/medicines" )
 @RequiredArgsConstructor
 @CrossOrigin("*")
 public class MedicineController   {
